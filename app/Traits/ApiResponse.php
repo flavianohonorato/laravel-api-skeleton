137bf7c8cc50507dc0c13f8de5e0193281a92189
flavianohonorato <flaviano.honorato@gmail.com>
2017-10-17 16:27:16 -0300

    added  trait for responses, messages, get collections, filter and sort data, paginate, etc

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace iChance\Traits;
+
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+trait ApiResponse
+{
+    /**
+     * @param $data
+     * @param $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    private function successResponse($data, $code)
+    {
+        return response()->json($data, $code);
+    }
+
+    /**
+     * @param $message
+     * @param $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function errorResponse($message, $code)
+    {
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+
+    /**
+     * @param $message
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function showMessage($message, $code = 200)
+    {
+        return $this->successResponse(['data' => $message], $code);
+    }
+
+    /**
+     * @param Collection $collection
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function getAll(Collection $collection, $code = 200)
+    {
+        if ($collection->isEmpty()){
+            return $this->successResponse(['data' => $collection], $code);
+        }
+        $transformer = $collection->first()->transformer;
+
+        $collection = $this->filterData($collection, $transformer);
+        $collection = $this->sortData($collection, $transformer);
+        $collection = $this->paginate($collection);
+
+        return $this->successResponse($collection, $code);
+    }
+
+    /**
+     * @param Model $model
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function getOne(Model $model, $code = 200)
+    {
+        return $this->successResponse(['data' => [$model]], $code);
+    }
+
+    /**
+     * @param Collection $collection
+     * @param $transformer
+     * @return Collection|static
+     */
+    public function filterData(Collection $collection, $transformer)
+    {
+        foreach (request()->query() as $query => $value) {
+            $attribute = $transformer::originalAttribute($query);
+            if (isset($attribute, $value)) {
+                $collection = $collection->where($attribute, $value);
+            }
+        }
+
+        return $collection;
+    }
+
+    /**
+     * @param Collection $collection
+     * @param $transformer
+     * @return Collection
+     */
+    protected function sortData(Collection $collection, $transformer)
+    {
+        if (request()->has('sort_by')){
+            $attribute = $transformer::originalAttribute(request()->sort_by);
+            $collection = $collection->sortBy->{$attribute};
+        }
+
+        return $collection;
+    }
+
+    /**
+     * @param Collection $collection
+     * @return LengthAwarePaginator
+     */
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50',
+        ];
+
+        Validator::validate(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 15;
+        if (request()->has('per_page')) {
+            $perPage = (int)request()->per_page;
+        }
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+        $paginated->appends(request()->all());
+
+        return $paginated;
+    }
+}
