ff207c6dd1c7d14bbfa8c51d5d5d7c3281929fd7
flavianohonorato <flaviano.honorato@gmail.com>
2017-10-18 10:50:32 -0300

    adding JWT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class AuthController extends Controller
+{
+    use ApiResponse;
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $rules = [
+            'name'      => 'required|max:255',
+            'email'     => 'required|email|max:255|unique:users',
+            'password'  => 'required|confirmed|min:6',
+        ];
+
+        $input = $request->only(
+            'name', 'email', 'password', 'password_confirmation'
+        );
+        $validator = Validator::make($input, $rules);
+
+        if($validator->fails()) {
+            $error = $validator->messages()->toJson();
+            return $this->errorResponse($error);
+        }
+        $name       = $request->name;
+        $email      = $request->email;
+        $password   = $request->password;
+        $user       = User::create([
+                        'name' => $name,
+                        'email' => $email,
+                        'password' => Hash::make($password)
+        ]);
+        $verification_code = str_random(30);
+
+        DB::table('user_verifications')->insert([
+            'user_id'   =>  $user->id,
+            'token'     =>  $verification_code
+        ]);
+        $subject = "Por favor verifique seu endereço de email";
+
+        Mail::send('emails.user-verificationß', [
+            'name'              => $name,
+            'verification_code' => $verification_code
+        ],
+        function($mail) use ($email, $name, $subject){
+            $mail->from(getenv('FROM_EMAIL_ADDRESS'), config('app.name'));
+            $mail->to($email, $name);
+            $mail->subject($subject);
+        });
+        return $this->showMessage('Obrigado por inscrever-se! Verifique seu e-mail para completar seu cadastro.');
+    }
+
+    /**
+     * @param $verification_code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function verifyUser($verification_code)
+    {
+        $check = DB::table('user_verifications')->where('token', $verification_code)->first();
+        if(!is_null($check)){
+            $user = User::find($check->user_id);
+            if($user->is_verified == 1){
+                return $this->showMessage('Sua conta já foi verificada antes');
+            }
+            $user->update(['is_verified' => 1]);
+            DB::table('user_verifications')->where('token',$verification_code)->delete();
+            return $this->showMessage('Você verificou com sucesso seu endereço de e-mail');
+        }
+        return $this->errorResponse("O código de verificação é inválido", 304);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $rules = [
+            'email'     => 'required|email',
+            'password'  => 'required',
+        ];
+        $input = $request->only('email', 'password');
+
+        $validator = Validator::make($input, $rules);
+        if($validator->fails()) {
+            $error = $validator->messages()->toJson();
+            return $this->errorResponse($error);
+        }
+        $credentials = [
+            'email'         => $request->email,
+            'password'      => $request->password,
+            'is_verified'   => 1
+        ];
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return $this->errorResponse("Credenciais inválidas. Certifique-se de inserir as informações certas e verificou seu endereço de e-mail", 401);
+            }
+        } catch (JWTException $e) {
+            return $this->errorResponse("Não foi possível criar token", 500)
+        }
+        return $this->getOne(['token' => $token]);
+    }
+
+    /**
+     * Invalidate the token, so user cannot use it anymore
+     * They have to relogin to get a new token
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request) {
+        $this->validate($request, ['token' => 'required']);
+        try {
+            JWTAuth::invalidate($request->input('token'));
+            return $this->showMessage("Logou efetudo com sucesso");
+        } catch (JWTException $e) {
+            return $this->errorResponse("Falha ao sair", 500);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function recover(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return $this->errorResponse("Endereço de e-mail não encontrado.", 401);
+        }
+        try {
+            Password::sendResetLink($request->only('email'), function (Message $message) {
+                $message->subject('Seu link de redefinição de senha');
+            });
+        } catch (\Exception $e) {
+            $error_message = $e->getMessage();
+            return $this->errorResponse($error_message, 401);
+        }
+        return $this->showMessage("Um e-mail de recuperação de senha foi enviado! Por favor verifique seu email");
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('register', 'AuthController@register');
+Route::post('login', 'AuthController@login');
+Route::post('recover', 'AuthController@recover');
+
+Route::group(['middleware' => ['jwt.auth']], function() {
+    Route::get('logout', 'AuthController@logout');
+    Route::get('test', function(){
+        return response()->json(['foo'=>'bar']);
+    });
 });
